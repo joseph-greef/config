71cca5c56a32ba00d5fd6971dac469b2c7548981
Joseph Greef <joseph.k.greef@gmail.com>
2021-04-15 17:37:08 -0700

    Clean up xmonad.hs and add autolaunching programs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,59 +1,60 @@
 import XMonad
 
-import XMonad.Layout.Tabbed
-import XMonad.Layout.Accordion
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Spiral
-import XMonad.Layout.ThreeColumns
-import XMonad.Layout.HintedGrid
-import XMonad.Layout.Mosaic
-import XMonad.Util.EZConfig
-import qualified XMonad.StackSet as W
 import XMonad.Actions.GroupNavigation
-import XMonad.Hooks.EwmhDesktops
+import XMonad.Actions.SpawnOn
 import XMonad.Actions.SwapWorkspaces
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.Tabbed
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ThreeColumns
+import XMonad.Util.EZConfig
 
--- Make multi float by default so all the little windows go above it and don't
--- screw with tiling. Downside: need to manually tile the first window
-myManageHook = composeAll
-    [ className =? "Multi" --> doFloat
-    ]
-    
--- some layouts I like
-mylayoutHook = ThreeCol 2 (3/100) (1/4) ||| 
+import qualified XMonad.StackSet as W
+
+myTerminal = "/home/llamanaru/.cargo/bin/alacritty"
+
+myLayoutHook = ThreeCol 2 (3/100) (1/4) |||
                tabbedBottom shrinkText def |||
-               Tall 1 (3/100) (1/2) 
-        
--- set up my customizations
+               Tall 1 (3/100) (1/2)
+
+myStartupHook = do
+    spawnOn "1" myTerminal
+    spawnOn "1" myTerminal
+    spawnOn "1" myTerminal
+    spawnOn "1" myTerminal
+    spawnOn "3:firefox" "firefox"
+    spawnOn "6:chromium" "chromium"
+    spawnOn "9:discord" "discord"
+    spawnOn "9:discord" "pavucontrol"
+
+myExtraWorkspaces = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"]
+myExtraWorkspaceKeys = [xK_0] ++ [xK_F1 .. xK_F12]
+
+myWorkspaces = ["1","2","3:firefox","4","5","6:chromium","7","8","9:discord"] ++ myExtraWorkspaces
+myWorkspaceKeys = [xK_1 .. xK_9]
+
+myAdditionalKeys =
+    [
+        ((mod1Mask, key), (windows $ W.greedyView ws))
+        | (key,ws) <- (zip myExtraWorkspaceKeys myExtraWorkspaces)
+    ] ++ [
+        ((mod1Mask .|. shiftMask, key), (windows $ W.shift ws))
+        | (key,ws) <- (zip myExtraWorkspaceKeys myExtraWorkspaces)
+    ] ++ [
+        ((mod1Mask .|. mod4Mask, key), windows $ swapWithCurrent ws)
+        | (key, ws) <- (zip (myWorkspaceKeys ++ myExtraWorkspaceKeys) (myWorkspaces ++ myExtraWorkspaces))
+    ]
+
 main=do
     xmonad $ ewmh defaultConfig
-        { 
-            terminal    = "/home/jgreef/.cargo/bin/alacritty"
-            , layoutHook = mylayoutHook
+        {
+            borderWidth = 0
+            , layoutHook = myLayoutHook
             , logHook = historyHook
-            , manageHook = myManageHook 
+            , manageHook = manageSpawn <+> manageHook def
             , modMask = mod1Mask
+            , startupHook = myStartupHook
+            , terminal    = myTerminal
             , workspaces = myWorkspaces
-            , borderWidth = 0
         } `additionalKeys` myAdditionalKeys
 
--- I never use the workspace names so 1-22 suffices for me
-myWorkspaces = ["1","2","3","4","5","6","7","8","9"] ++ (map snd myExtraWorkspaces)
-allWorkspaces = zip myWorkspaces ([xK_1 .. xK_9] ++ [xK_0] ++ [xK_F1 .. xK_F12])
-
-myWorkspaceKeys = [xK_F1 .. xK_F12]
-myExtraWorkspaces = [(xK_0, "10"),(xK_F1, "11"),(xK_F2, "12"),(xK_F3, "13"),(xK_F4, "14"),(xK_F5, "15"),(xK_F6, "16"),(xK_F7, "17"),(xK_F8, "18"),(xK_F9, "19"),(xK_F10, "20"),(xK_F11, "21"),(xK_F12, "22")]
-
--- set up changing to extra workspaces and moving windows to them
-myAdditionalKeys = 
-      [ -- ... some more keys ...
-      ] ++ [
-        ((mod1Mask, key), (windows $ W.greedyView ws))
-        | (key,ws) <- myExtraWorkspaces
-      ] ++ [
-        ((mod1Mask .|. shiftMask, key), (windows $ W.shift ws))
-        | (key,ws) <- myExtraWorkspaces
-      ] ++ [
-        ((mod1Mask .|. mod4Mask, k), windows $ swapWithCurrent i)
-            | (i, k) <- allWorkspaces
-      ]
